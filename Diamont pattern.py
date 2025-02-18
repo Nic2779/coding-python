75f09a87004fef646768fd679300f41273ceaa00
Nic2779 <nicdimitriou@icloud.com>
2025-02-18 16:44:13 +0200

    Diamond patternt.py

---
+++ Diamont pattern.py
@@ -0,0 +1,14 @@
+rowSize = int(input("enter the number of rows"))
+if rowSize%2==0:
+  halfDiamRow = int(rowSize/2)
+else:
+  space = halfDiamRow-1
+for i in range(1, halfDiamRow+1):
+  for j in range(1, space+1):
+    print(end=" ")
+space = space-1
+num = 1
+for j in range(2*i-1 ):
+   print(end=str(num))
+   num = num+1
+print()
