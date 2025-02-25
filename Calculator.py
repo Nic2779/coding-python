aa7c581546b2cf9134d934dbaf409e99d0c50615
Nic2779 <nicdimitriou@icloud.com>
2025-02-25 16:44:30 +0200

    Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,20 @@
+def add(P, Q):
+    return P + Q
+def subtract(P, Q):
+    return P - Q
+def multyply(P, Q):
+    return P * Q
+def divide(P, Q):
+    return P / Q
+    
+    print("Please enter choice a/ b/ c/ d")
+    print("a. Add")
+    print("b. subtract")
+    print("c. multyply")
+    print("d. divide")
+
+    choice = input("Please enter choice (a/ b/ c/ d):")
+
+    num_1 = int (input ("Please enter the first number:"))
+    num_2 = int (input ("Please enter second number:"))
+    
---
+++ Function.py
@@ -0,0 +1,4 @@
+def Nic_Ang():
+   print("Hi nic")
+   print("Are you good?")
+Nic_Ang()
